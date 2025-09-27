d4be38016972bf849b51cba4e6458d0fb423d431
miriansanches <sanchesmirian489@gmail.com>
2025-09-27 11:35:15 -0300

    Update query.py

--- query.py
+++ query.py
@@ -1,22 +1,20 @@
-# pip install mysql-connector-python
-# pip install pandas
-
 import mysql.connector
 import pandas as pd
+import streamlit as st
 
 def conexao(query):
-
-    con = mysql.connector.connect(
-        host="projetointegrador-grupo2.mysql.database.azure.com",
-        port="3306",
-        user="projeto2",
-        password="senai%40134",
-        db="db_sensor"
-
-    )
-
-    df = pd.read_sql(query, con)
-    # Executar o SQL e armazenar o resultado no dataframe
-
-    con.close()
-    return df
+    try:
+        con = mysql.connector.connect(
+            host="projetointegrador-grupo2.mysql.database.azure.com",
+            port="3306",
+            user="projeto2",
+            password="senai%40134",
+            database="db_sensor",
+            ssl_ca="DigiCertGlobalRootG2.crt.pem"  # Caminho para o certificado
+        )
+        df = pd.read_sql(query, con)
+        con.close()
+        return df
+    except Exception as e:
+        st.error(f"Erro na conexão com o banco: {e}")
+        return pd.DataFrame()
